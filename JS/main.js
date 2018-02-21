d205db4ec85f9bd01c420e3c656c882e3a7bd492
Vanja Ivanovic <vanja.i@hotmail.com>
2018-02-21 13:32:36 +0100

    knappar för att ta bort och bocka av

--- JS/main.js
+++ JS/main.js
@@ -5,7 +5,17 @@ function addTask() {
   var task = document.getElementById("todoInput").value;
   var text = document.createTextNode(task);
   var newTask = document.createElement("li");
+  var span = document.createElement("span");
+  var remove = document.createTextNode("\u00D7");
+  var buttonDone = document.createElement("button");
+  var buttontxt = document.createTextNode("done");
 
+  span.className = "delete";
+
+  newTask.appendChild(span);
+  span.appendChild(remove);
+  newTask.appendChild(buttonDone);
+  buttonDone.appendChild(buttontxt);
   newTask.appendChild(text);
   list.appendChild(newTask);
 }
